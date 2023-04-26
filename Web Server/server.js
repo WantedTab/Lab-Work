60156d44ee85c9711f2a01b2060686c97d33d9da
WantedTab <130139910+WantedTab@users.noreply.github.com>
2023-04-26 20:35:58 +0300

    Add files via upload

---
+++ Web Server/server.js
@@ -0,0 +1,26 @@
+﻿const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Hello from my web server</h1>');
+        res.end();
+    } else if (req.url === '/SPRING.html') {
+        fs.readFile('primavara.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url === '/WINTER.html') {
+        fs.readFile('iarna.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+});
+
+server.listen(8080, () => {
+    console.log('Server-ul functioneaza');
+});
